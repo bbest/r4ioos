b53593e232f79d050917067ee7621e10b3f70ff0
Ben Best <ben@ecoquants.com>
2019-10-08 06:33:07 -0500

    + index.html

--- make.R
+++ make.R
@@ -1,3 +1,4 @@
-rmarkdown::render("test.Rmd", output_format="html_document")
+rmarkdown::render("README.md", output_format = "html_document", output_file = "index.html")
+rmarkdown::render( "test.Rmd", output_format = "html_document")
 #rmarkdown::render("test.Rmd", output_format="pdf_document")
 #rmarkdown::render("test.Rmd", output_format="word_document")
