58a6917f24bfcc5bcae6f6cdd1e13a17b3b76097
Ben Best <ben@ecoquants.com>
2019-10-08 06:29:45 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # r4ioos
-demo of R for automation &amp; reporting
+demo of R for automation & reporting
+
+- [test.html](test.html)
+- [test.docx](test.docx)
+- [test.pdf](test.pdf)
---
+++ make.R
@@ -0,0 +1,3 @@
+rmarkdown::render("test.Rmd", output_format="html_document")
+#rmarkdown::render("test.Rmd", output_format="pdf_document")
+#rmarkdown::render("test.Rmd", output_format="word_document")
